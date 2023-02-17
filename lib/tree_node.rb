f3945cad1b83009ccdb5501490b0a24ad77e9170
Taisia Karaseva <taisiat@gmail.com>
2023-02-17 14:16:56 -0800

    Add initialize logic and parent= logic to node class

--- lib/tree_node.rb
+++ lib/tree_node.rb
@@ -1,3 +1,19 @@
 class PolyTreeNode
   
+    attr_reader :value, :parent, :children
+    def initialize(val)
+        @value = val
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(parent_val)
+        unless parent_val.nil?
+            if @parent != nil
+                @parent.children.delete(self)
+            end
+            @parent = parent_val
+            parent_val.children << self
+        end
+    end
 end
